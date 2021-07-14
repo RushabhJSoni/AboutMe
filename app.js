75fb3370f8806035ca4cfef14efeab190124133e
slayerr1 <slayer.r1@gmail.com>
2021-07-14 19:27:05 +0530

    added Js file and function

--- app.js
+++ app.js
@@ -1,1 +1,68 @@
 'use strict'
+
+
+let userName = prompt('Hi there! waht\'s your name?');
+console.log(userName);
+
+alert('Welcome to my site, here\'s a fun game for you to know me. Please answer with a yes or no');
+
+let knowMyName = prompt('Do you know my name?')
+
+theyKnow = knowMyName.toLocaleLowerCase();
+console.log(theyKnow);
+
+if (theyKnow === 'yes' || theyKnow === 'y') {
+  alert('congratulations you are awesome!');
+} else if (theyKnow === 'no' || theyKnow === 'n') {
+  alert(' :( ');
+} else {
+  alert('Please read the instructions!');
+}
+
+
+let planet = prompt('Do you know which planet i\'m from?');
+
+newPlanet = planet.toLocaleLowerCase();
+
+console.log(newPlanet);
+
+
+if (newPlanet === 'yes' || newPlanet === 'y') {
+  alert('Phew i\'m glad you know its called Earth!');
+} else if (newPlanet === 'no' || newPlanet === 'n') {
+  alert('Nope its not Tatooine...');
+} else {
+  alert('Please read the instructions!');
+}
+
+let country = prompt('Do you know the name of country I hail from?');
+
+yesCountry = country.toLocaleLowerCase();
+
+console.log(yesCountry);
+
+if (yesCountry === 'yes' || yesCountry === 'y') {
+  alert('oh yea, wow you\'re smart ' + userName);
+} else if (yesCountry === 'no' || yesCountry === 'n') {
+  alert('i\'m sure you can do better ' + userName);
+} else {
+  alert('Please read the instructions!');
+}
+
+let vehicle = prompt('Do you know the model of my atmospheric vehicle of choice?');
+
+theyKnowVehicle = vehicle.toLocaleLowerCase();
+
+console.log(theyKnowVehicle);
+
+
+if (theyKnowVehicle === 'yes' || theyKnowVehicle === 'y') {
+  alert('Ding Ding Ding yup, its the A320! join me for a joyride someday.');
+} else if (theyKnowVehicle === 'no' || theyKnowVehicle === 'n') {
+  alert('Fyi the shuttle is my spare ship of choice ;) ');
+} else {
+  alert('Please read the instructions!');
+}
+
+alert('Thank you for playing my game and getting to know me better ' + userName + '.' + ' Welcome to my world');
+
